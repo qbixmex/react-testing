673534b6016f70f575bd406c63ba22b1814d6127
Daniel González Briseño <qbixmex@gmail.com>
2023-07-16 23:45:11 -0700

    query-by

--- src/components/skills/skills.component.tsx
+++ src/components/skills/skills.component.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import { type SkillsProps } from "./skills";
 
 const Skills = ({ skills }: SkillsProps) => {
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
   return (
     <>
       <ul>
         {skills.map(skill => <li key={skill}>{skill}</li>)}
       </ul>
+
+      {
+        isLoggedIn ? (
+          <p>You're logged in!</p>
+        ) : (
+          <button onClick={() => setIsLoggedIn(true)}>Login</button>
+        )
+      }
+
     </>
   );
 };
--- src/tests/components/skills/skills.test.tsx
+++ src/tests/components/skills/skills.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Skills } from "../../../components";
 
-const skills = ['html', 'sass', 'sass', 'javascript', 'typescript'];
+const skills = ['html', 'css', 'sass', 'javascript', 'typescript'];
 
 describe('Tests on <Skills />', () => {
   test('Should render correctly', () => {
@@ -14,5 +14,17 @@ describe('Tests on <Skills />', () => {
     render(<Skills skills={skills} />);
     const listItemElements = screen.getAllByRole('listitem');
     expect(listItemElements).toHaveLength(skills.length);
+  });
+
+  test('Should render a button', () => {
+    render(<Skills skills={skills} />);
+    const button = screen.getByRole('button', { name: 'Login' });
+    expect(button).toBeInTheDocument();
+  });
+
+  test('Should not render login successful text by default', () => {
+    render(<Skills skills={skills} />);
+    const text = screen.queryByText(/^You're/, { exact: false });
+    expect(text).not.toBeInTheDocument();
   });
 });
